0d2c5246af0f6fad3c585dbc3e937e2ed66a402f
masaki0914 <m.msk0914@i.softbank.jp>
2021-11-13 09:45:51 +0900

    first commit

---
+++ components/PageFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="text-muted">
+    <div class="container">
+      <p>©キカガク</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+  footer {
+    border-top: 1px solid #efefef;
+    padding-top: 10px;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <section class="jumbotron text-center">
+      <div class="container">
+        <h1 class="jumbotron-heading">キカガクの商品紹介サイト</h1>
+        <p class="lead text-muted">
+          短期のハンズオンセミナーや長期コースなど様々な講座を用意しています。
+        </p>
+        <p>
+          <a href="https://www.kikagaku.co.jp/" class="btn btn-primary my-2">
+            キカガクのホームページ
+          </a>
+        </p>
+      </div>
+    </section>
+    <main>
+      <div class="album py-5">
+        <div class="container">
+          <div class="row">
+            <!-- 商品カード -->
+            <ProductCard 
+             v-for="(product, index) in products"
+             :key="index"
+             :name="product.name"
+             :img="product.img"
+             :text="product.text"
+             :url="products.url"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    async asyncData() {
+      const url = 'https://api.steinhq.com/v1/storages/616d044dc582292380ab0c35/product'
+      const { data } = await axios.get(url)
+      return { products: data }
+    },
+  }
+</script>
